79cc14cd0da49adc512f17daa8daab9822b21d0c
Bruno Keller <brunochaveskeller1102@outlook.com>
2024-04-17 11:04:16 +0100

    Change the letter from the exercise 7.

--- ex007.py
+++ ex007.py
@@ -1,4 +1,4 @@
-#Desenvolva um programa que leia as duas notas de um aluno. Caucule e mostre a sua media.
+#Desenvolva um programa que leia as duas notas de um aluno. Calcule e mostre a sua media.
 n1 = float(input('Primeira nota:'))
 n2 = float(input('Segunda nota:'))
 m = (n1 + n2) / 2
